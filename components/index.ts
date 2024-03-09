4e0dcd1ce982ce3395bf93d6f94115171f9c3189
TatsuyaMatsuoka0904 <fuzzbass72@gmail.com>
2024-03-09 09:44:19 +0900

    nav del

--- components/index.ts
+++ components/index.ts
@@ -1,12 +1,10 @@
 import Hero from './Hero';
-import Navbar from './Navbar';
 import Footer from './Footer';
 import Collect from './Collect';
 
 // コンポーネントのindex　ここからアクセス
 export {
     Hero,
-    Navbar,
     Footer,
     Collect
 }
